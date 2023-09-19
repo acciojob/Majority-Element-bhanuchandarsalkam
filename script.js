e5c03fa7be477981155b0ac73e5343debf3cec55
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-19 09:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let arr=[2,1,2];
+let a=arr.length;
+
+for(let i=0;i<arr.length;i++){
+	let count=0;
+for(let j=i+1;j<arr.length;j++){
+	if(arr[i]==arr[j]){
+		count++;
+	}
+	if(count>floor(a/2)){
+		console.log(arr[i]);
+		break;
+	}
+}
+}
